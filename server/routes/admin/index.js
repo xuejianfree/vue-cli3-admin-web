1ec175590806ff5708bf617933fb217547d8afeb
ling <xuejianeie@163.com>
2019-07-31 01:20:29 +0800

    2.3 分类列表组件

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -6,5 +6,9 @@ module.exports = app => {
     const model = await Category.create(req.body)
     res.send(model)
   })
+  router.get('/categories', async (req, res) => {
+    const items = await Category.find().limit(10)
+    res.send(items)
+  })
   app.use('/admin/api', router)
 }
