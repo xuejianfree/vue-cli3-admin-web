741e70a317dbfec823847ffce9876d091a20b69b
ling <xuejianeie@163.com>
2019-08-02 14:24:01 +0800

    子分类

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,8 +2,22 @@
   <div class="about">
     <h1>{{id ? '编辑' : '新建'}}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-row>
+          <el-col :span="8">
+            <el-select v-model="model.parent">
+              <el-option v-for="item in parents" :key="item._id"
+              :label="item.name" :value="item._id"></el-option>
+            </el-select>
+          </el-col>
+        </el-row>
+      </el-form-item>
       <el-form-item label="名称">
-        <el-row><el-col :span="8"><el-input v-model="model.name" placeholder="请输入内容"></el-input></el-col></el-row>
+        <el-row>
+          <el-col :span="8">
+            <el-input v-model="model.name" placeholder="请输入内容"></el-input>
+          </el-col>
+        </el-row>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
@@ -18,37 +32,43 @@ export default {
     // 接收url里的id参数
     id: {}
   },
-  data(){
+  data() {
     return {
-      model: {}
-    }
+      model: {},
+      parents: []
+    };
   },
   methods: {
-    async save(){
-      let res
-      if(this.id){
+    async save() {
+      let res;
+      if (this.id) {
         // 新增和编辑共用Edit组件
         // 有id
-        res = await this.$http.put(`categories/${this.id}`, this.model)
+        res = await this.$http.put(`categories/${this.id}`, this.model);
       } else {
         // 没有id就是新增
-        res = await this.$http.post('categories', this.model)
+        res = await this.$http.post("categories", this.model);
       }
-      this.$router.push('/categories/list')
+      this.$router.push("/categories/list");
       this.$message({
-        type: 'success',
-        message: '保存成功'
-      })
+        type: "success",
+        message: "保存成功"
+      });
     },
-    async fetch(){
-      const res = await this.$http.get(`categories/${this.id}`)
-      this.model =  res.data
+    async fetch() {
+      const res = await this.$http.get(`categories/${this.id}`);
+      this.model = res.data;
+    },
+    async fetchParents() {
+      const res = await this.$http.get(`categories`);
+      this.parents = res.data;
     }
   },
-  created(){
+  created() {
+    this.fetchParents();
     // 有id说明是点击编辑，才执行
     // 要是编辑分类，就把数据显示在输入框里
-    this.id && this.fetch()
+    this.id && this.fetch();
   }
-}
+};
 </script>
