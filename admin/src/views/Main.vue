daff7d9ad4f91d74c1a51077299b2ab57df0eb59
ling <xuejianeie@163.com>
2020-03-10 12:42:43 +0800

    移动端基础样式

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -60,16 +60,11 @@
             <el-dropdown-item>删除</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-        <span>Ling</span>
+        <span>xuejianfree</span>
       </el-header>
 
       <el-main>
         <router-view></router-view>
-        <!-- <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140"></el-table-column>
-          <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-          <el-table-column prop="address" label="地址"></el-table-column>
-        </el-table>-->
       </el-main>
     </el-container>
   </el-container>
